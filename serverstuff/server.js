728aeb4d90f2afade276fa2ad8bdf699d689521d
Holger Ludvigsen <holger.ludvigsen@bekk.no>
2018-05-09 19:29:12 +0200

    Legger på CORS-headere

--- serverstuff/server.js
+++ serverstuff/server.js
@@ -18,6 +18,12 @@ const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
