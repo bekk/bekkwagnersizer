03a7049de7177d222c11d92534134d610fe0f50e
Joakim Tysseng <joakim@tysseng.no>
2018-05-30 22:51:43 +0200

    Added CORS headers to lysserver

--- serverstuff/lysserver.js
+++ serverstuff/lysserver.js
@@ -11,6 +11,15 @@ var strip = null;
 
 let MODE = 'NONE';
 
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  );
+  next();
+});
+
 app.get('/spin', function(req, res) {
   MODE = 'SPIN';
   let color = [req.query.r, req.query.g, req.query.b];
