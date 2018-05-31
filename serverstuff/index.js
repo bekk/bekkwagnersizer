f7241a5ecd18f1216c01cc7fd3a5ef8c585249a8
Holger Ludvigsen <holger.ludvigsen@bekk.no>
2018-05-31 09:01:05 +0200

    La til flere forms for rapid debugging

--- serverstuff/index.js
+++ serverstuff/index.js
@@ -1,19 +1,22 @@
-const imageForm = document.getElementById('imageForm');
 
-imageForm.onsubmit = (e) => {
-  e.preventDefault();
-  const file = e.target.file.files[0];
-  const formData = new FormData();
-  formData.append('image', file);
+for (let i = 1; i <= 3; i++) {
+  const imageForm = document.getElementById('imageForm'+i);
 
-  fetch('/image', {
-    method: 'POST',
-    body: formData
-  }).then(
-    response => response
-  ).then(
-    success => console.log(success)
-  ).catch(
-    error => console.log(error)
-  );
+  imageForm.onsubmit = (e) => {
+    e.preventDefault();
+    const file = e.target.file.files[0];
+    const formData = new FormData();
+    formData.append('image', file);
+
+    fetch('/image', {
+      method: 'POST',
+      body: formData
+    }).then(
+      response => response
+    ).then(
+      success => console.log(success)
+    ).catch(
+      error => console.log(error)
+    );
+  }
 }
